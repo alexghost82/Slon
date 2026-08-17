ef1b906a44ce5b4c3a2c551bda6edd1bfaa1891a
alexghost82 <alex@ghost-il.com>
2026-08-17 10:27:19 +0300

    feat(tools): add safety-gated tool executor

--- mark/tools/__init__.py
+++ mark/tools/__init__.py
@@ -2,6 +2,7 @@
 
 from mark.tools.contracts import ArtifactRef, ToolResult, ToolSpec
 from mark.tools.errors import DuplicateToolError, ToolRegistryError, UnknownToolError
+from mark.tools.executor import ToolExecutor
 from mark.tools.registry import ToolRegistry
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     "ToolRegistryError",
     "ToolResult",
     "ToolSpec",
+    "ToolExecutor",
     "UnknownToolError",
 ]
